ddac3cc9378b4d472d5986329da2ab9a8dcc4abb
Talon <talontalon24@gmail.com>
2019-02-10 00:10:55 +0100

    better formatting, putting actual functionality into another function

--- actor_pairings.py
+++ actor_pairings.py
@@ -1,28 +1,24 @@
-"""media where 2 actors or voice actors participated together. uses imdb links"""
+"""Get media in which specified actors worked together. Uses IMDB"""
 
 import argparse
 import functools
 
 import requests
 import bs4 as bs
-# import ipdb
 
-def main():
-    """main"""
-    urls = ["https://www.imdb.com/name/nm0354937/", # Jennifer Hale
-            "https://www.imdb.com/name/nm1035752/"] # Mark Meer
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("urls", nargs="*", default=urls, help="List of imbd urls"
-                        " or keys (after /name/ in the url) to process")
-    arg = parser.parse_args()
-    arg.urls[:] = ["https://www.imdb.com/name/" + url
-                   if not url.startswith("http") else url
-                   for url in arg.urls]
-
+def find_media(urls):
+    """
+    Get all media in which an actor participated and
+    return the intersection of all. Also get actor names.
+    Args:
+        urls: Iterable of URLs that direct to IMDB imdb.com/name/ pages
+    Returns:
+        actor_names: List of Actor Names taken from the url
+        intersecion: Set of Media in which all actors participated
+    """
     media_sets = []
     actor_names = []
-    for url in arg.urls:
+    for url in urls:
         text = requests.get(url).text
         soup = bs.BeautifulSoup(text, "lxml")
         actor_names.append(soup.find("span", class_="itemprop").text)
@@ -30,11 +26,25 @@ def main():
         media = {row.find("a").text for row in rows}
         media_sets.append(media)
 
-    actors = " and ".join(actor_names)
-    print("--- Media in which {} cooperated:".format(actors))
     intersection = functools.reduce(set.intersection, media_sets)
-    # intersection = media_sets[0].intersection(media_sets[1])
-    for medium in sorted(intersection):
+    return actor_names, intersection
+
+def main():
+    """get arguments, execute, print"""
+    urls = ["https://www.imdb.com/name/nm0354937/", # Jennifer Hale
+            "https://www.imdb.com/name/nm1035752/"] # Mark Meer
+    # ^ Default values
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("urls", nargs="*", default=urls, help="List of IMDB "
+                        "urls or keys (after /name/ in the url) to process")
+    arg = parser.parse_args()
+    arg.urls[:] = ["https://www.imdb.com/name/" + url
+                   if not url.startswith("http") else url
+                   for url in arg.urls]
+    actors, media = find_media(arg.urls)
+    print("--- Media in which {} cooperated:".format(" and ".join(actors)))
+    for medium in sorted(media):
         print(medium)
 
 if __name__ == '__main__':
